a321100cacfe22eb375698f37732189dbce71940
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-07-08 23:43:15 +0800

    Add test case of isValidInput with input 1,2

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,7 @@ const main = require('../main');
 it ('should return true when validate input given 1,1', () => {
     expect(main.isValidInput(1, 1)).toBe(true);
 });
+
+it ('should return true when validate input given 1,2', () => {
+    expect(main.isValidInput(1, 2)).toBe(true);
+});
